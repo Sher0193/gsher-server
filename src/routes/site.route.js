3c3b74f12bfab63ac6e4ed0685745a45af1063bc
Sher0193 <ovidvscape@gmail.com>
2020-11-13 21:29:19 -0500

    add protected analytics page

--- src/routes/site.route.js
+++ src/routes/site.route.js
@@ -1,4 +1,5 @@
 module.exports = (app) => {
+  const express = require("express");
   const site = require("../controllers/site.controller.js");
   const auth = require("../controllers/login.controller.js");
 
@@ -11,4 +12,9 @@ module.exports = (app) => {
   app.post(siteUrl + "about", auth.authenticate, site.postAbout);
 
   app.post(siteUrl + "statement", auth.authenticate, site.postStatement);
+
+  app.use("/api/analytics/", [
+    auth.authenticate,
+    express.static("private/report.html"),
+  ]);
 };
